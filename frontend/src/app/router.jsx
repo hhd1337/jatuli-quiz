31fc60c0eb84210f69b54fb835ef39233cd78acc
goeka <goeka1337@gmail.com>
2026-06-03 04:39:48 +0900

    feat(frontend/quiz): 사용하지 않는 FolderBrowserPage 제거
    - 홈화면에서 모든 depth를 나타내므로, FolderBrowserPage 자체를 제거

--- frontend/src/app/router.jsx
+++ frontend/src/app/router.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import AppShell from "./AppShell";
 
 import HomePage from "../pages/home/HomePage";
-import FolderBrowserPage from "../pages/folders/FolderBrowserPage";
 import QuizPlayPage from "../pages/quiz/QuizPlayPage";
 import QuizEditPage from "../pages/quiz/QuizEditPage";
 import NotFoundPage from "../pages/error/NotFoundPage";
@@ -13,7 +12,6 @@ export const router = createBrowserRouter([
     element: <AppShell />,
     children: [
       { path: "/", element: <HomePage /> },
-      { path: "/folders/:folderId", element: <FolderBrowserPage /> },
       { path: "/quiz/play", element: <QuizPlayPage /> },
       { path: "/quiz/:quizId/edit", element: <QuizEditPage /> },
       { path: "/test/health", element: <HealthCheckPage /> },
