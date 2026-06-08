b03beb2e7c19dbf214cfb8b364da740271216a23
goeka <goeka1337@gmail.com>
2026-06-08 10:35:07 +0900

    feat(backend/config): GitHub 일일 기록 업로드 설정 추가

---
+++ backend/src/main/java/com/hhd1337/jatuli_quiz/config/RestClientConfig.java
@@ -0,0 +1,14 @@
+package com.hhd1337.jatuli_quiz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
+}
