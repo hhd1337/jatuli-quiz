fbf7994364538c88972d56630c99a89db43cd2f8
goeka <goeka1337@gmail.com>
2026-06-08 18:53:52 +0900

    feat(backend/routine): 오늘 루틴 저장소 추가

---
+++ backend/src/main/java/com/hhd1337/jatuli_quiz/domain/routine/repository/RoutinePeriodRepository.java
@@ -0,0 +1,7 @@
+package com.hhd1337.jatuli_quiz.domain.routine.repository;
+
+import com.hhd1337.jatuli_quiz.domain.routine.entity.RoutinePeriod;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoutinePeriodRepository extends JpaRepository<RoutinePeriod, Long> {
+}
